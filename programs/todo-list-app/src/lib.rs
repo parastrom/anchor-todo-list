d99a3c755e7d4b06c672415d7bbda3b49df5e982
parastrom <adewoletomisin78@gmail.com>
2023-12-22 18:27:24 +0000

    new

--- programs/todo-list-app/src/lib.rs
+++ programs/todo-list-app/src/lib.rs
@@ -1,20 +1,40 @@
 use anchor_lang::prelude::*;
+declare_id!("9LZdb5Cxg2TowwxKYU1h4mzKQDfEC52AaUAjoqt5emDM");
 
-declare_id!("<PLACE YOUR ADDRESS HERE>");
+const DISCRIMINATOR: usize = 8;
+const PUBLIC_KEY_LENGTH: usize = 32;
+const BOOL_LENGTH: usize = 1;
+const TEXT_LENGTH: usize = 4 + 400 * 4; //400 char
+const TIMESTAMP_LENGTH: usize = 8;
+
+#[error_code]
+pub enum ErrorCode {
+    #[msg("Text too long")]
+    TextTooLong,
+}
+
+impl From<ErrorCode> for std::io::Error {
+    fn from(e: ErrorCode) -> Self {
+        std::io::Error::new(std::io::ErrorKind::Other, e.to_string())
+    }
+}
+
 
 #[program]
 pub mod todo_list_app {
+    use std::io::Result;
+
     use super::*;
 
     pub fn adding_task(ctx: Context<AddingTask>, text: String) -> Result<()> {
         let task = &mut ctx.accounts.task;
-        let author = &ctx.accounts.author; // The `author` account
-        let clock = Clock::get().unwrap(); // Getting the current timestamp
-        
+        let author = &ctx.accounts.author;
+        let clock = Clock::get().unwrap();
+
         if text.chars().count() > 400 {
             return Err(ErrorCode::TextTooLong.into());
         }
-        
+
         task.author = *author.key;
         task.is_done = false;
         task.created_at = clock.unix_timestamp;
@@ -24,33 +44,44 @@ pub mod todo_list_app {
 
     }
 
-    pub fn updating_task(ctx: Context<UpdatingTask>, is_done: bool) -> Result<()> {
+    pub fn updating_task(ctx:  Context<UpdatingTask>, is_done: bool) -> Result<()> {
         let task = &mut ctx.accounts.task;
-        let author = &ctx.accounts.author; // The `author` account
-        let clock = Clock::get().unwrap(); // Getting the current timestamp
-        
+        let author = &ctx.accounts.author;
+        let clock = Clock::get().unwrap();
         task.author = *author.key;
         task.is_done = is_done;
         task.updated_at = clock.unix_timestamp;
         Ok(())
-       
     }
 
     pub fn deleting_task(ctx: Context<DeletingTask>) -> Result<()> {
         let task = &mut ctx.accounts.task;
-        let author = &ctx.accounts.author; // The `author` account
-        let clock = Clock::get().unwrap(); // Getting the current timestamp
-        
+        let author = &ctx.accounts.author;
+        let clock = Clock::get().unwrap(); 
         task.author = *author.key;
         task.is_done = true;
         task.updated_at = clock.unix_timestamp;
         Ok(())
        
     }
+}
 
+#[account]
+pub struct Task {
+    pub author: Pubkey,  // Account which owns the task,
+    pub is_done: bool,   // whether the task is done or not,
+    pub text: String,    // Text of the task,
+    pub created_at: i64, // Timestamp for task creation
+    pub updated_at: i64, // Timestamp for task updates,
+}
 
-
-
+impl Task {
+    const LEN: usize = DISCRIMINATOR + // DISCRIMINATOR
+    PUBLIC_KEY_LENGTH + // author
+    BOOL_LENGTH + // is_done,
+    TEXT_LENGTH + // text,
+    TIMESTAMP_LENGTH + //created at,
+    TIMESTAMP_LENGTH; // updated_at
 }
 
 #[derive(Accounts)]
@@ -61,8 +92,6 @@ pub struct AddingTask<'info> {
     pub author: Signer<'info>,
     pub system_program: Program<'info, System>,
 }
-
-
 #[derive(Accounts)]
 pub struct UpdatingTask<'info> {
     #[account(mut, has_one = author)]
@@ -75,36 +104,4 @@ pub struct DeletingTask<'info> {
     #[account(mut, has_one = author)]
     pub task: Account<'info, Task>,
     pub author: Signer<'info>,
-}
-
-
-#[account]
-pub struct Task {
-    pub author: Pubkey,  // The account that owns the task
-    pub is_done: bool,   // Whether the task is done or not
-    pub text: String,    // The text of the task
-    pub created_at: i64, // The timestamp when the task was created
-    pub updated_at: i64, // The timestamp when the task was last updated
-}
-
-const DISCRIMINATOR: usize = 8;
-const PUBLIC_KEY_LENGTH: usize = 32;
-const BOOL_LENGTH: usize = 1;
-const TEXT_LENGTH: usize = 4 + 400 * 4; // 400 chars
-const TIMESTAMP_LENGTH: usize = 8;
-
-impl Task {
-    const LEN: usize = DISCRIMINATOR + // discriminator
-        PUBLIC_KEY_LENGTH + // author
-        BOOL_LENGTH + // is_done
-        TEXT_LENGTH +  // text
-        TIMESTAMP_LENGTH + // created_at
-        TIMESTAMP_LENGTH; // updated_at
-}
-
-
-#[error_code]
-pub enum ErrorCode {
-    #[msg("The text is too long")]
-    TextTooLong,
 }
--- tests/todo-list-app.ts
+++ tests/todo-list-app.ts
@@ -6,118 +6,36 @@ import { assert } from "chai";
 describe("todo-list-app", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
+
   const program = anchor.workspace.TodoListApp as Program<TodoListApp>;
+  
   const author = program.provider as anchor.AnchorProvider;
-  it("can create a task", async () => {
+  it("Can create a task" , async () => {
     const task = anchor.web3.Keypair.generate();
     const tx = await program.methods
-      .addingTask("You are awesome")
-      .accounts({
-        task: task.publicKey,
-        systemProgram: anchor.web3.SystemProgram.programId,
-      })
-      .signers([task])
-      .rpc();
+    .addingTask("First task")
+    .accounts({
+      task: task.publicKey,
+      systemProgram: anchor.web3.SystemProgram.programId,
+    })
+    .signers([task])
+    .rpc();
+
     console.log("Your transaction signature", tx);
 
     const taskAccount = await program.account.task.fetch(task.publicKey);
-    console.log("Your task", taskAccount);
+    console.log("Task account: ", taskAccount);
 
     assert.equal(
       taskAccount.author.toBase58(),
-      author.wallet.publicKey.toBase58()
+      author.wallet.publicKey.toBase58(),
     );
-    assert.equal(taskAccount.text, "You are awesome");
+
+    assert.equal(taskAccount.text, "First task");
     assert.equal(taskAccount.isDone, false);
     assert.ok(taskAccount.createdAt);
     assert.ok(taskAccount.updatedAt);
   });
-
-  // it("can create a new task from a different author", async () => {
-  //   const user = anchor.web3.Keypair.generate();
-
-  //   const signature = await program.provider.connection.requestAirdrop(
-  //     user.publicKey,
-  //     10000000000
-  //   );
-
-  //   const latestBlockHash =
-  //     await program.provider.connection.getLatestBlockhash();
-
-  //   await program.provider.connection.confirmTransaction({
-  //     blockhash: latestBlockHash.blockhash,
-  //     lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
-  //     signature,
-  //   });
-
-  //   const task = anchor.web3.Keypair.generate();
-
-  //   const tx = await program.methods
-  //     .addingTask("You are more than awesome")
-  //     .accounts({
-  //       task: task.publicKey,
-  //       author: user.publicKey,
-  //       systemProgram: anchor.web3.SystemProgram.programId,
-  //     })
-  //     .signers([task, user])
-  //     .rpc();
-
-  //   console.log("Your transaction signature", tx);
-  //   const taskAccount = await program.account.task.fetch(task.publicKey);
-  //   console.log("Your task", taskAccount);
-
-  //   assert.equal(taskAccount.author.toBase58(), user.publicKey.toBase58());
-  //   assert.equal(taskAccount.text, "You are more than awesome");
-  //   assert.equal(taskAccount.isDone, false);
-  //   assert.ok(taskAccount.createdAt);
-  //   assert.ok(taskAccount.updatedAt);
-  // });
-
-  // it("can fetch all tasks", async () => {
-  //   const tasks = await program.account.task.all();
-  //   console.log("Your tasks", tasks);
-  // });
-
-  // it("can filter tasks by author", async () => {
-  //   const authorPublicKey = author.wallet.publicKey;
-  //   console.log("authorPublicKey", authorPublicKey.toBase58());
-  //   const tasks = await program.account.task.all([
-  //     {
-  //       memcmp: {
-  //         offset: 8,
-  //         bytes: authorPublicKey.toBase58(),
-  //       },
-  //     },
-  //   ]);
-
-  //   assert.equal(tasks.length, 1);
-  // });
-
-  // it("can update a task to done", async () => {
-  //   const task = anchor.web3.Keypair.generate();
-  //   // create a new task
-  //   // fetch it from the same address
-  //   // then update
-  //   const tx = await program.methods
-  //     .updatingTask(true)
-  //     .accounts({
-  //       task: task.publicKey,
-  //       author: author.wallet.publicKey,
-  //     })
-  //     .signers([author])
-  //     .rpc();
-
-  //   console.log("Your transaction signature", tx);
-
-  //   const taskAccount = await program.account.task.fetch(task.publicKey);
-  //   console.log("Your task", taskAccount);
-
-  //   assert.equal(
-  //     taskAccount.author.toBase58(),
-  //     author.wallet.publicKey.toBase58()
-  //   );
-  //   assert.equal(taskAccount.isDone, true);
-  // });
 
   // it("cannot create a task with more than 400 characters", async () => {
   //   const task = anchor.web3.Keypair.generate();
@@ -135,5 +53,24 @@ describe("todo-list-app", () => {
   //   } catch (err) {
   //     assert.equal(err.toString(), "Error: failed to send transaction");
   //   }
+  // });
+
+  // it("Can update a task", async () => {
+  //   const task = anchor.web3.Keypair.generate();
+  //   const update_tx = await program.methods.updatingTask(true)
+  //   .accounts({
+  //     task: task.publicKey,
+  //     author: author.wallet.publicKey,
+  //   })
+  //   .signers([author])
+  //   .rpc();
+
+  //   console.log("Update signature", update_tx);
+
+  //   const taskAccount = await program.account.task.fetch(task.publicKey);
+  //   console.log("Task account: ", taskAccount);
+
+  //   assert.equal(taskAccount.isDone, true);
+  //   assert.ok(taskAccount.updatedAt > taskAccount.createdAt);
   // });
 });
